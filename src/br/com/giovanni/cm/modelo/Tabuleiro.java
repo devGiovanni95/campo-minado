76f80b2540610d7e4783d6ce656864ee81a8651f
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-05-13 22:25:13 -0300

    Campo minado
    criando tabuleiro

--- src/br/com/giovanni/cm/modelo/Tabuleiro.java
+++ src/br/com/giovanni/cm/modelo/Tabuleiro.java
@@ -1,5 +1,94 @@
 package br.com.giovanni.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class Tabuleiro {
 
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    public  void abrir(int linha,int coluna){
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.abrir());
+    }
+
+    public  void alterarMarcacao(int linha,int coluna){
+        campos.parallelStream()
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                .ifPresent(c -> c.alternarMarcacao());
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas ;linha++){
+            for (int coluna = 0; coluna < colunas; coluna++){
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        for (Campo c1: campos){
+            for (Campo c2: campos){
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+        long minasArmadas = 0;
+        Predicate<Campo> minado = c -> c.isMinado();
+
+        do {
+            minasArmadas = campos.stream().filter(minado).count();
+            int aleatorio = (int)(Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+        }while (minasArmadas < minas);
+    }
+
+
+    public boolean objetivoAlcancado() {
+       return campos.stream().allMatch( c -> c.objetivoAlcancado());
+    }
+
+    public void reiniciar(){
+        campos.stream().forEach( c -> c.reiniciar());
+        sortearMinas();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+        for (int line = 0; line < linhas; line++){
+            for (int colun = 0; colun < colunas; colun++){
+                sb.append(" ");
+                sb.append(campos.get(i));
+                sb.append(" ");
+                i++;
+
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
